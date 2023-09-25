9318ba47ef9bf1f6433dd076303f2c08c06d644d
BatsyKun <119741750+KoochieKun@users.noreply.github.com>
2023-09-25 09:50:35 +0530

    handsOn

--- Bootstrap/bootstrap/src/App.jsx
+++ Bootstrap/bootstrap/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Breadcrumb } from "react-bootstrap";
+import Dropdownn from "./Dropdown";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light" size="lg">
+        Light
+      </Button>{" "}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+          Library
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Data</Breadcrumb.Item>
+      </Breadcrumb>
+      <Dropdownn></Dropdownn>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
